e7b8938ce45f6bcab6159d3e274880794fd07da2
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-05-26 18:41:48 +0700

    study EditText

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         TextView tvHello = (TextView) findViewById(R.id.tvHello);
         tvHello.setMovementMethod(LinkMovementMethod.getInstance());
         tvHello.setText(Html.fromHtml("<b>Hello</b> <i>World</i> <font color=\"#fafafa\">La la la</font> <a href=\"https://www.google.com/\">Google" ));
+
+        EditText editTextHello = (EditText) findViewById(R.id.editTextHello);
+        editTextHello.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // Copy text in EditText to TextView
+                    tvHello.setText(editTextHello.getText());
+                    //Handle
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
