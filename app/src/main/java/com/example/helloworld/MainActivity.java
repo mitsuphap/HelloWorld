e1827dfd674e31cbdf760e90dc87f05644ece6c8
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-05-26 18:13:01 +0700

    study TextView

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvHello = (TextView) findViewById(R.id.tvHello);
+        tvHello.setMovementMethod(LinkMovementMethod.getInstance());
+        tvHello.setText(Html.fromHtml("<b>Hello</b> <i>World</i> <font color=\"#fafafa\">La la la</font> <a href=\"https://www.google.com/\">Google" ));
     }
 }
