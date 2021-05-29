36f322cf77f96ea0838d16d7613d093906dd9186
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-05-29 23:59:28 +0700

    Add Create CustomView file

---
+++ app/src/main/java/com/example/helloworld/CustomView.java
@@ -0,0 +1,48 @@
+package com.example.helloworld;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+public class CustomView extends View {
+
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(21)
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint p = new Paint();
+        p.setColor(0xFFFF0000); // #AARRGGBB
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+    }
+}
