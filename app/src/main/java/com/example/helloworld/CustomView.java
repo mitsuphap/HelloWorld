081d3cc559331170084a96527e277a757b0ef126
mitsuphap <sitanan.mitsuphap@gmail.com>
2021-05-31 21:02:28 +0700

    Studt CustomView

--- app/src/main/java/com/example/helloworld/CustomView.java
+++ app/src/main/java/com/example/helloworld/CustomView.java
@@ -6,6 +6,9 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -14,6 +17,8 @@ import androidx.annotation.RequiresApi;
 public class CustomView extends View {
 
     private boolean isBlue = false;
+    private boolean isDown = false;
+    private GestureDetector gestureDetector;
 
     public CustomView(Context context) {
         super(context);
@@ -40,7 +45,50 @@ public class CustomView extends View {
     }
 
     private void init() {
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                // Decide: Care or not care?
+                return true;
+            }
 
+            @Override
+            public void onShowPress(MotionEvent e) {
+                // Depend on onDown if true do
+                getParent().requestDisallowInterceptTouchEvent(true);
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                isBlue = !isBlue;
+                invalidate();
+                return true;
+            }
+        });
+        //Enable Click Node
+        setClickable(true);
+        // After this call, if it's not clickable, it will be clickable
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void initWithAttrs(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
@@ -68,5 +116,43 @@ public class CustomView extends View {
         }
 
         canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+
+        if (isDown) {
+            p.setColor(0xFF00FF00);
+
+            //convert dp to px
+
+            float px = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    5,
+                    getContext().getResources().getDisplayMetrics()
+            );
+            p.setStrokeWidth(px);
+
+            //Draw Line
+            canvas.drawLine(0, getMeasuredHeight(), getMeasuredWidth(), 0, p);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                isDown = true;
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                isDown = false;
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+                isDown = false;
+                invalidate();
+                return true;
+        }
+        return super.onTouchEvent(event);
     }
 }
